d2f355d2b3e5e5833cb5aae56c4ee9d2eac6ffe2
Ronnie Rios <ronniesmail14@gmail.com>
2021-09-05 16:07:46 -0500

    added mongoose connection to server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/binzone', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+//routes
 app.get('/', (req, res)=> {
    const code =`Welcome to the BinZone, 
 follow the commands in the top right corner 
@@ -11,5 +21,11 @@ to create a new file to share with others`
 
 app.get('/new', (req, res) => {
     res.render('new')
+})
+
+app.post('/save', (req, res) => {
+    const value = req.body.value
+    console.log(value)
+
 })
 app.listen(3000)
